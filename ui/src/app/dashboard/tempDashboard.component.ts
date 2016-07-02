0575ce774b824d1c853f9ba62c114734ac34b757
Peter Vanco <petervanco@petervanco.sk>
2016-07-02 22:01:22 +0200

    temp dashboard + chrome debugging

--- ui/src/app/dashboard/tempDashboard.component.ts
+++ ui/src/app/dashboard/tempDashboard.component.ts
@@ -1,8 +1,10 @@
-import {Component, ViewChild, AfterViewInit} from '@angular/core';
+import {Component, ViewChildren, AfterViewInit} from '@angular/core';
 import {Observable} from 'rxjs/Rx';
 import {AbstractDashboard} from './abstractDashboard.component';
 import {ChartDashlet} from './dashlet/chartDashlet.component';
 import {ChartDashletRangeSelector} from './dashlet/control/chartDashletRangeSelector.component';
+
+declare var ResizeSensor: any;
 
 @Component({
     selector: 'temp-dashboard',
@@ -13,27 +15,44 @@ export class TempDashboard extends AbstractDashboard implements AfterViewInit {
 
     private name: string;
 
-    @ViewChild('indoorTemperatureChart')
-    indoorTemperatureChart: ChartDashlet;
-
-    @ViewChild('outdoorTemperatureChart')
-    outdoorTemperatureChart: ChartDashlet;
+    @ViewChildren(ChartDashlet)
+    charts: ChartDashlet[];
 
     ngAfterViewInit() {
         super.ngAfterViewInit();
-        this.setFullHeight();
-        $(window).resize(e => this.setFullHeight());
+        this.equalizeChartHeights();
     }
 
-    private setFullHeight() {
-        $(".box-body-temp-indoor").height(($(".content-wrapper").height() - 150) / 2);
-        $(".box-body-temp-outdoor").height(($(".content-wrapper").height() - 150) / 2);
-        if (this.indoorTemperatureChart.getPlot()) {
-            this.indoorTemperatureChart.getPlot().reflow();
-        }
-        if (this.outdoorTemperatureChart.getPlot()) {
-            this.outdoorTemperatureChart.getPlot().reflow();
-        }
+    public equalizeChartHeights() {
+
+        let content: any = $(".content");
+        let contentWrapper: any = $(".content-wrapper");
+
+        let equalize = () => {
+
+            let thisDashboard = this;
+
+            var neg = $('.main-header').outerHeight() + $('.main-footer').outerHeight();
+            var window_height = $(window).height();
+
+            //Destroy if it exists
+            content.slimScroll({
+                destroy: true
+            }).height("auto");
+            //Add slimscroll
+            content.slimScroll({
+                height: (window_height - neg) + 'px'
+            });
+
+            let contentHeight = contentWrapper.height();
+            contentWrapper.find(".box").each(function (index) {
+                let headerHeight = $(this).find(".box-header").height();
+                $(this).find(".box-body").height(contentHeight / thisDashboard.charts.length - (75 + headerHeight));
+            });
+            this.charts.forEach(chart => chart.getPlot() && chart.getPlot().reflow());
+        };
+        equalize();
+        new ResizeSensor(contentWrapper, equalize);
     }
 
 }

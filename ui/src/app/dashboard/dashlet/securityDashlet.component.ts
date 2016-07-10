5d1c547845032f453569cd9fadfeab3e5da5068a
Peter Vanco <petervanco@petervanco.sk>
2016-07-10 13:23:27 +0200

    security dashlet

---
+++ ui/src/app/dashboard/dashlet/securityDashlet.component.ts
@@ -0,0 +1,67 @@
+import {Component, AfterViewInit, ElementRef, Input, OnInit, OnDestroy, ChangeDetectorRef} from '@angular/core';
+import {Observable, Subscription} from 'rxjs/Rx';
+import {HemiService} from '../../service/hemiService.service';
+import {DataModel, Camera, CameraSnapshot} from '../../service/data.model';
+import {AbstractDashlet} from './abstractDashlet.component';
+import {Subject, BehaviorSubject} from 'rxjs/Rx'
+
+@Component({
+    selector: 'security-dashlet',
+    templateUrl: '/src/tpl/dashboard/dashlet/securityDashlet.component.html',
+	styles: [`
+    canvas {
+        width: 100%;
+        height: 100%;
+    }
+	`]
+})
+export class SecurityDashlet extends AbstractDashlet<DataModel> implements OnInit {
+
+	private canvas: HTMLCanvasElement;
+
+	constructor(protected _hemiService: HemiService,
+		protected element: ElementRef) {
+		super(_hemiService);
+	}
+
+	extractData(model: DataModel) {
+		return model;
+	}
+
+	handleData(data: DataModel) {
+
+	}
+
+	ngOnInit() {
+		super.ngOnInit();
+		this.canvas = $(this.element.nativeElement).find('canvas')[0] as any;
+	}
+
+    public redraw() {
+
+		let canvas = this.canvas;
+        let ctx = canvas.getContext("2d");
+
+        canvas.width = canvas.clientWidth;
+        canvas.height = canvas.clientHeight;
+
+        let width = canvas.width;
+        let height = canvas.height;
+
+        // ctx.scale(0.5, 0.5);
+
+        this.drawLine(ctx, width * .1, height * .1, width * .9, height * .1, 'ff0000', 2);
+        this.drawLine(ctx, width * .9, height * .1, width * .9, height * .9, '00ff00', 2);
+
+    }
+
+    private drawLine(ctx: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number, color: string = "000000", width: number = 1) {
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.lineWidth = width;
+        ctx.strokeStyle = '#' + color;
+        ctx.stroke();
+    }
+
+}
--- ui/src/app/dashboard/securityDashboard.component.ts
+++ ui/src/app/dashboard/securityDashboard.component.ts
@@ -1,27 +1,19 @@
-import {Component, AfterViewInit, ElementRef} from '@angular/core';
+import {Component, AfterViewInit, ViewChild} from '@angular/core';
 import {AbstractDashboard} from './abstractDashboard.component';
+import {SecurityDashlet} from './dashlet/securityDashlet.component'
 import {Observable} from 'rxjs/Rx';
 
 @Component({
     selector: 'security-dashboard',
-    templateUrl: '/src/tpl/dashboard/securityDashboard.component.html',
-    directives: [],
-    styles: [`
-    canvas {
-        width: 100%;
-        height: 100%;
-    }
-    `]
+    template: '<security-dashlet></security-dashlet>',
+    directives: [SecurityDashlet]
 })
 export class SecurityDashboard extends AbstractDashboard implements AfterViewInit {
 
-    constructor(protected element: ElementRef) {
-        super();
-    }
+    @ViewChild(SecurityDashlet)
+    private dashlet: SecurityDashlet;
 
     ngAfterViewInit() {
-
-        let canvas: HTMLCanvasElement = $(this.element.nativeElement).find('canvas')[0] as any;
 
         super.initSlimScroll(contentWrapper => {
             let thisDashboard = this;
@@ -31,33 +23,13 @@ export class SecurityDashboard extends AbstractDashboard implements AfterViewIni
                 $(this).find(".box-body").height(contentHeight - (80 + headerHeight));
             });
 
-            this.redraw(canvas);
+            this.dashlet.redraw();
         });
 
         // AbstractDashboard.addResizeListener($('.content-wrapper'), () => {
         //     let contentPadding = $(".content").outerHeight() - $(".content").height();
         //     $(".box-body").outerHeight($(".content-wrapper").height() - $(".box-header").outerHeight() - contentPadding - 20);
         // });
-    }
-
-    public redraw(canvas: HTMLCanvasElement) {
-
-        let ctx = canvas.getContext("2d");
-
-        canvas.width = canvas.clientWidth;
-        canvas.height = canvas.clientHeight;
-
-        let width = canvas.width;
-        let height = canvas.height;
-
-        ctx.beginPath();
-        ctx.translate(0.5, 0.5);
-        ctx.moveTo(width / 10, height / 10);
-        ctx.lineTo(width - width / 10, height - height / 10);
-        ctx.lineWidth = 2;
-        ctx.strokeStyle = '#ff0000';
-        ctx.stroke();
-
     }
 
 }
